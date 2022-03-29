928e11b5d945ca2287d193dd4e1deaa3ee6e9093
parkksss <yesleee9211@gmail.com>
2022-03-29 13:26:02 +0900

    add function - addnote

--- src/Add.js
+++ src/Add.js
@@ -2,25 +2,26 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Add = (props) => {
-  const input_t = React.useRef(null); 
-  const input_d = React.useRef(null); 
-  const input_u = React.useRef(null); 
+  // const input_t = React.useRef(null); 
+  // const input_d = React.useRef(null); 
+  // const input_u = React.useRef(null); 
   // console.log(input_t.current.value, input_d.current.value, input_u.current.value);
 
   return (
-    <Note>
-      <h3>단어 추가하기</h3>
+    <div> </div>
+    // <Note>
+    //   <h3>단어 추가하기</h3>
       
-      <AddForm>
-        <label>단어</label>
-        <input type="text" ref={input_t}/>
-        <label>설명</label>
-        <input type="text" ref={input_d}/>
-        <label>구문</label>
-        <input type="text" ref={input_u}/>
-        <button type="submit" value="Submit">추가하기</button>
-      </AddForm>
-    </Note>
+    //   <AddForm>
+    //     <label>단어</label>
+    //     <input type="text" ref={input_t}/>
+    //     <label>설명</label>
+    //     <input type="text" ref={input_d}/>
+    //     <label>구문</label>
+    //     <input type="text" ref={input_u}/>
+    //     <button type="submit" value="Submit">추가하기</button>
+    //   </AddForm>
+    // </Note>
   );
 }
 
--- src/App.js
+++ src/App.js
@@ -6,16 +6,35 @@ import Add from './Add'
 
 function App() {
 
-  const notes = {
+  const [notes, setNotes] = React.useState({
     title: ['split() 메서드', 'join() 메서드', 'map() 메서드'],
     desc: ['지정한 구분자를 이용하여 여러 개의 문자열로 나눈다', '배열의 모든 요소를 연결해 하나의 문자열로 만든다', '배열 내의 모든 요소 각각에 대하여 주어진 함수를 호출한 결과를 모아 새로운 배열을 반환한다'],
     use: ['str.split([separator[, limit]])', 'arr.join([separator])', 'arr.map(callback(currentValue[, index[, array]])[, thisArg])']
-  };
+  });
+  // const notes = {
+  //   title: ['split() 메서드', 'join() 메서드', 'map() 메서드'],
+  //   desc: ['지정한 구분자를 이용하여 여러 개의 문자열로 나눈다', '배열의 모든 요소를 연결해 하나의 문자열로 만든다', '배열 내의 모든 요소 각각에 대하여 주어진 함수를 호출한 결과를 모아 새로운 배열을 반환한다'],
+  //   use: ['str.split([separator[, limit]])', 'arr.join([separator])', 'arr.map(callback(currentValue[, index[, array]])[, thisArg])']
+  // };
   // const notes = [
   //   {title: 'split() 메서드', 
   //   desc: '지정한 구분자를 이용하여 여러 개의 문자열로 나눈다', 
   //   use: 'str.split([separator[, limit]])'},
   // ] 
+  const input_t = React.useRef(null); 
+  const input_d = React.useRef(null); 
+  const input_u = React.useRef(null); 
+
+  const addNote = () => {
+    const new_title = input_t.current.value;
+    const new_desc = input_d.current.value;
+    const new_use = input_u.current.value;    
+    setNotes({ title: [...notes.title, new_title], desc: [...notes.desc, new_desc], use: [...notes.use, new_use]});
+  };
+  const handleSubmit = (e) => {
+    // submit을 할 때 페이지 자체가 새로고침이 되는 것을 막음
+    e.preventDefault();
+  };
 
   return (
     <AppWrap className="App">
@@ -26,8 +45,22 @@ function App() {
       <Container>
         <Notes notes={notes}/>
       </Container>
-      <Add/>
-      <button>추가하기</button>
+      {/* <Add/> */}
+
+      <Note>
+        <h3>단어 추가하기</h3>
+        
+        <AddForm onSubmit={handleSubmit}>
+          <label>단어</label>
+          <input type="text" ref={input_t}/>
+          <label>설명</label>
+          <input type="text" ref={input_d}/>
+          <label>구문</label>
+          <input type="text" ref={input_u}/>
+          <button value="Submit" onClick={addNote}>추가하기</button>
+        </AddForm>
+      </Note>
+      {/* <button>추가하기</button> */}
     </AppWrap>
   );
 }
@@ -53,5 +86,26 @@ const Container = styled.div`
   max-width: 500px;
   margin-top: 25px;
 `;
+
+const Note = styled.div`
+  width: 95%;
+  max-width: 500px;
+  height: 500px;
+  margin-top: 25px;
+  border: 2px solid #ddd;
+  border-radius: 10px;
+  h3 {
+    text-align: center;
+  }
+`;
+const AddForm = styled.form`
+  padding: 0 20px 20px;
+  display: flex;
+  flex-direction: column;
+  button {
+    margin-top: 10px;
+  }
+`;
+
 
 export default App;
